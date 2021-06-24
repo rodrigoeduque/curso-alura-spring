6b3c36f9f02b06dc39ee19f24076477d2e3e8e76
Rodrigo Eustaquio Duque <rodrigoeduque@hotmail.com>
2021-06-24 16:23:39 -0300

    testes-automatizados

---
+++ src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
@@ -0,0 +1,29 @@
+package br.com.alura.forum.repository;
+
+import br.com.alura.forum.modelo.Curso;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@DataJpaTest
+@RunWith(SpringRunner.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
+public class CursoRepositoryTest {
+
+    @Autowired
+    private CursoRepository repository;
+
+    @Test
+    public void DeveriaCarregarUmCursoPeloNome() {
+        String nomeCurso = "Spring Boot";
+        Curso curso = repository.findByNome(nomeCurso);
+        Assert.assertNotNull(curso);
+        Assert.assertEquals(nomeCurso, curso.getNome());
+    }
+}
